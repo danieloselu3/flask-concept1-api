b3c2d1f858bb8fc7952ec7af9c5b922f72169bb4
danieloselu3 <doselu3@gmail.com>
2023-10-01 23:51:36 +0300

    added logic for when the request method is GET

--- app.py
+++ app.py
@@ -2,13 +2,45 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+user_data = [
+    {
+        'user_id': 1,
+        'user_name': 'User1',
+        'prompt': 'Please provide feedback on our new product design.'
+    },
+    {
+        'user_id': 2,
+        'user_name': 'User2',
+        'prompt': 'Describe your favorite travel destination.'
+    },
+    {
+        'user_id': 3,
+        'user_name': 'User3',
+        'prompt': 'Share your thoughts on the latest technology trends.'
+    },
+    {
+        'user_id': 4,
+        'user_name': 'User4',
+        'prompt': 'Write a short story about a detective solving a mystery.'
+    },
+    {
+        'user_id': 5,
+        'user_name': 'User5',
+        'prompt': 'Discuss the impact of climate change on global ecosystems.'
+    }
+]
+
+
 @app.route("/", methods=["GET", "POST"])
 def index(user, prompt):
     if request.method == "GET":
-        pass
+        if user_data > 0:
+            return jsonify(user_data)
+        else:
+            return "There's no users, for now!", 404
 
     if request.method == "POST":
-        pass
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
